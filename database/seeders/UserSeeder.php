ccfebb36af58ecc9f24d629ed5073bbfd3c8e7a7
unknown <kamrankhan3.61k@gmail.com>
2025-10-08 21:02:57 +0500

    adding the whole code

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Create test user
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password123'),
+        ]);
+
+        // Create additional test users
+        User::factory(5)->create();
     }
 }
